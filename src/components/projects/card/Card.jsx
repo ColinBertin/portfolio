aa31d18e1aa23122238bb28efd154ee499529b39
Colin Bertin <colinbertin12@gmail.com>
2022-03-24 18:45:31 +0900

    card display not finish

--- src/components/projects/card/Card.jsx
+++ src/components/projects/card/Card.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './Card.scss';
 
+import traq from './traq.png';
+
 const Card = () => {
   return (
     <div className='card'>
-      <div className="preview">
-        <img src="" alt="" />
+      <div className="card-preview">
+        <img src={traq} alt="traq" />
       </div>
-      <div className="info">
+      <div className="card-info">
         <h3>TraQ</h3>
         <ul>
-          <li className='language'><i class="devicon-rails-plain-wordmark"></i></li>
-          <li className='language'><i class="devicon-javascript-plain"></i></li>
-          <li className='language'><i class="devicon-heroku-original-wordmark"></i></li>
-          <li className='language'><i class="devicon-adonisjs-original"></i></li>
-          <li className='language'><i class="devicon-adonisjs-original"></i></li>
+          <li className='card-language'><i class="devicon-rails-plain-wordmark"></i></li>
+          <li className='card-language'><i class="devicon-javascript-plain"></i></li>
+          <li className='card-language'><i class="devicon-heroku-original-wordmark"></i></li>
+          <li className='card-language'><i class="devicon-adonisjs-original"></i></li>
+          <li className='card-language'><i class="devicon-adonisjs-original"></i></li>
         </ul>
       </div>
     </div>
