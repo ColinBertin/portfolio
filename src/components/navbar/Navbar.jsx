c814e9577e9ab2ace9579e948a97de57d6c3a9f7
Colin Bertin <colinbertin12@gmail.com>
2022-03-29 10:50:01 +0900

    react-scroll & link added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa'
-import './Navbar.scss'
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.scss';
+import { Link } from 'react-scroll';
 
 const Navbar = (props) => {
   return (
     <div className="navbar">
       {/* Menu */}
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+      <ul className=''>
+        <li>
+          <Link to='banner' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='projects' smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
 
       {/* Hamburger */}
       <div>
