e7a19a05fab20710d901d9b0d5b224c414f2194c
Colin Bertin <colinbertin12@gmail.com>
2022-03-30 11:12:31 +0900

    color setup

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './Banner.scss';
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 import me from '../../assets/me.jpeg';
 
 const Banner = (props) => {
   return (
     <div className='banner'>
-      <div className="photo">
+      <div className="banner-photo">
         <img src={me} alt="Me" />
       </div>
-      <div className="intro">
-        <h1>Hi, I am <span className='blue'>Colin</span>!</h1>
+      <div className="banner-intro">
+        <p className='banner-pink'>Hi, my name is</p>
+        <h1 className='banner-h1'>Colin Bertin</h1>
+        <h2 className='banner-h2'>I'm a Full-Stack Developer </h2>
         <p>I am a Full-Stack web developer. I am looking to leverage my skills in Ruby on Rails and JavaScript.</p>
+        <button>
+          View Work
+          < HiArrowNarrowRight />
+        </button>
       </div>
     </div>
   );
