4644be3f40dd55122dbc56a09468f24ac6fae402
Colin Bertin <colinbertin12@gmail.com>
2022-03-31 11:50:53 +0900

    deploying

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import Banner from './components/banner/Banner';
 import About from './components/about/About';
-import Skills from './components/skills/Skills';
+// import Skills from './components/skills/Skills';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
 
@@ -13,7 +13,7 @@ function App() {
       < Banner />
       <main>
         < About />
-        < Skills />
+        {/* < Skills /> */}
         < Projects />
         < Contact />
       </main>
