02b01d77efe98bfbb25377bf9f597c97435e5920
Colin Bertin <colinbertin12@gmail.com>
2022-04-26 15:39:12 +0900

    small issue textarea solved

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = (props) => {
       <form method="POST" action="https://getform.io/f/79f566d4-9401-42ec-9f41-92c02f90ad5e">
         <input type="text" name="name" placeholder="Name" required/>
         <input type="email" name="email" placeholder="Email" required/>
-        <textarea name="message" id="" rows="10" placeholder="Message" minLength="10" required></textarea>
+        <textarea name="message" id="textarea" rows="10" placeholder="Message" minLength="10" required></textarea>
         <button className='btn'>Let's Collaborate</button>
       </form>
     </section>
