672a2139bcf7d3d9a452c65048ef8c7bf9441c8c
Colin Bertin <colinbertin12@gmail.com>
2022-12-01 14:25:36 +0900

    started to apply typescript

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Contact.scss';
 
-const Contact = ({theme}) => {
+const Contact = () => {
   return (
     <section className='contact'>
       <h2>Contact</h2>
       <form method="POST" action="https://getform.io/f/79f566d4-9401-42ec-9f41-92c02f90ad5e">
         <input type="text" name="name" placeholder="Name" required/>
         <input type="email" name="email" placeholder="Email" required/>
-        <textarea name="message" id="textarea" rows="10" placeholder="Message" minLength="10" required></textarea>
+        <textarea name="message" id="textarea" rows={10} placeholder="Message" minLength={10} required></textarea>
         <button className='btn'>Let's Collaborate</button>
       </form>
     </section>
