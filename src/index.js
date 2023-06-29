aecb6459af3fc52a2f7a1575bba0aaefdfbbe90c
Colin Bertin <48044238+ColinBertin@users.noreply.github.com>
2023-06-29 18:37:14 +0900

    add redirecting page to github page 404

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import PageNotFound from "./components/404/PageNotFound";
-
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/404" element={<PageNotFound />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
