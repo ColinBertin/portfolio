aa8eb995ba2ed1bc5b3e700a83a81960e14666b6
Colin Bertin <colinbertin12@gmail.com>
2024-05-22 21:42:30 +0900

    fix duplicate sentence

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,20 +11,20 @@ const About = () => {
           Hi! I'm Colin, nice to meet you. Please take a look around.
         </p>
         <p className="about-main-text">
-          Passionate about building software and applications, I am a
-          36-year-old Belgian. Based in Yokohama since 2018, I have been honing
-          my web development skills. With a keen interest in the web industry, I
-          am eager to collaborate on exciting projects. Feel free to contact me
-          for more information!<span> </span>
+          I'm a web developer based in Yokohama, Japan, with a passion for
+          crafting user-centric web experiences. Since 2018, I've honed my
+          skills in building dynamic and responsive websites and applications.
+          I'm a strong believer in the power of collaboration and bring a
+          positive and results-oriented attitude to every project.<br />
           <Link
             className="about-contact-link"
             to="contact"
             smooth={true}
             duration={500}
           >
-            contact me
+            Contact
           </Link>
-          <span> </span>for more info!
+          {" "}me for more info!
         </p>
       </div>
     </section>
