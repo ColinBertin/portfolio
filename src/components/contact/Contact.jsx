242e7f412d1ff94dfee2de7e64f8d5b7e0800ca7
ColinBertin <colinbertin12@gmail.com>
2025-09-11 22:32:55 +0900

    fix input fields bg color

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,16 +9,17 @@ const Contact = ({ theme }) => {
         method="POST"
         action="https://getform.io/f/79f566d4-9401-42ec-9f41-92c02f90ad5e"
       >
-        <input type="text" name="name" placeholder="Name" required />
-        <input type="email" name="email" placeholder="Email" required />
+        <input className={`bg-${theme}`} type="text" name="name" placeholder="Name" required />
+        <input className={`bg-${theme}`} type="email" name="email" placeholder="Email" required />
         <textarea
+          className={`bg-${theme}`}
           name="message"
           id="textarea"
           rows="10"
           placeholder="Message"
           minLength="10"
           required
-        ></textarea>
+        />
         <button className={`btn btn-${theme}`}>Let's Collaborate</button>
       </form>
     </section>
